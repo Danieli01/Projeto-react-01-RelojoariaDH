1a0e678a550f910343d052db7ce2f2e4899b2325
Danieli Jaqueline do Nascimento <djaqueline.nascimento@gmail.com>
2023-03-21 20:56:36 -0300

    add repository remote

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/master.css';
+import Web from './components/Web';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Web />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
